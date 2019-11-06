6b6ffc6a0ae157fafaa3eaf5720fa0e8d325be8c
Yagmur Dalman <yagmur.dalman@gmail.com>
2019-11-06 16:30:10 +0100

    feat: finish colored latex table creation

--- Assignment#2/Assignment#2.py
+++ Assignment#2/Assignment#2.py
@@ -4,26 +4,50 @@ import re
 # result = re.findall(r"\\begin{tabular\}(?:{.*\})?\s*(?:\s*(.*?)\\\\\s*)*\\end{tabular}", latex_input)
 
 def change_color(latex_input, pairs, colors):
-    table = extract_table(latex_input)
+    (formatting,table) = extract_table(latex_input)
 
-    for pair in pairs:
-       print(table[pair[0]][pair[1]])
+    for idx, pair in enumerate(pairs):
+        content = table[pair[0]][pair[1]]
+        color = colors[idx]
+
+        colored_content = r"\cellcolor{}{}{}{}".format('{', color, '}', content)
+
+        table[pair[0]][pair[1]] = colored_content
+
+    constructed_table = construct_latex(formatting,table)
+    return constructed_table
 
 
+
+def construct_latex(formatting,table):
+    table = [" & ".join(row) for row in table]
+
+    table = ' \\\\\n'.join(table)
+
+    table = '{}{}\n{} \\\\\n{}'.format('\\begin{tabular}',formatting, table, '\\end{tabular}')
+
+    return table
+
+# Expected Output:
+
+# \begin{tabular}
+#  0.02 & \cellcolor{#549085}0.88 & 0.11 \\
+#  0.65 & 0.02 & \cellcolor{#123456}0.94 \\
+#  0.31 & 0.52 & 0.50 \\
+# \end{tabular}
 
 def extract_table(latex_input):
-    rowsStr = re.findall(r"\\begin\{tabular\}(?:\{.*\})?(.*)\\\\\s*\\end{tabular}", latex_input, flags=re.S)[0]
-    print(rowsStr)
-
+    # findall gives all the matches as a list. by using [0] we take only the first tuple.
+    (formatting,rowsStr) = re.findall(r"\\begin{tabular\}({.*\})?(.*)\\\\\s*\\end{tabular}", latex_input, flags=re.S)[0]
     rows = re.split(r'\\\\', rowsStr)
 
-    # rows = [row.strip() for row in rows]
-
-    # print(rows)
-
     table = [re.split(r'&', row) for row in rows]
-    print(table)
-    return table
+
+    for i, x in enumerate(table):
+        for j, a in enumerate(x):
+            table[i][j] = a.strip()
+
+    return (formatting, table)
 
 
 latex_table = r"""
@@ -34,4 +58,6 @@ latex_table = r"""
  0.31 & 0.52 & 0.50 \\
 
 \end{tabular}"""
-change_color(latex_table, [[1, 2], [0, 1]], ['#123456', '#549085'])
+final_table = change_color(latex_table, [[1, 2], [0, 1]], ['#123456', '#549085'])
+print(final_table)
+
