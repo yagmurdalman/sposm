b67958a221c9cb8063bf6df9a3474383903aee85
Yagmur Dalman <yagmur.dalman@gmail.com>
2019-11-13 13:10:41 +0100

    feat: input validation

--- Assignment#2/Assignment#2.py
+++ Assignment#2/Assignment#2.py
@@ -4,8 +4,20 @@ import re
 # result = re.findall(r"\\begin{tabular\}(?:{.*\})?\s*(?:\s*(.*?)\\\\\s*)*\\end{tabular}", latex_input)
 
 def change_color(latex_input, pairs, colors):
+    assert len(pairs) == len(colors), "The number of pairs and colors must be the same!"
+
+    assert type(pairs) == list, "The type of pairs must be list!"
+
+    assert type(colors) == list, "The type of colors must be list!"
+
+    assert "\hline" not in latex_input, "Currently latex tables with '\hline' are not supported!"
+
+
     (formatting,table) = extract_table(latex_input)
+
     for idx, pair in enumerate(pairs):
+        checkValidPair(table, pair)
+
         row_idx, col_idx = pair[0]-1, pair[1]-1
         content = table[row_idx][col_idx]
         color = colors[idx]
@@ -49,15 +61,27 @@ def extract_table(latex_input):
 
     return (formatting, table)
 
+def checkValidPair(table, pair):
+    if pair[0] < 1 or pair[1] < 1:
+        raise ValueError("Pairs must contain only positive integers. Found: {}".format(pair))
+    if pair[0] > len(table):
+        raise ValueError("Pairs must be in the range. Found: {}".format(pair))
+    if pair[1] > len(table[pair[0]-1]):
+        raise ValueError("Pairs must be in the range. Found: {}".format(pair))
+
+
+
 
 latex_table = r"""
 \begin{tabular}{rrr}
-
+ Col1 & Col2 & Col3 \\
  0.02 & 0.88 & 0.11 \\
  0.65 & 0.02 & 0.94 \\
  0.31 & 0.52 & 0.50 \\
 
 \end{tabular}"""
-final_table = change_color(latex_table, [[1, 2], [3, 1], [1, 1]], ['#123456', '#549085', '#549085'])
+final_table = change_color(latex_table, [[1, 2], [2, 2], [3,1]], ['red', 'blue', 'green'])
 print(final_table)
+
+
 
